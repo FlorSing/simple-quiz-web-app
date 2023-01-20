0989829a0a21f93174fdcfdc8cc2853b87473a0c
FlorSing <fpsingcak@gmail.com>
2023-01-20 10:06:56 +0000

    save scores.js changes

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,8 +1,8 @@
 //create list of high scores
 var scoreElement =  document.getElementById('highscores');
 
-let scoresList = [];
-let score = 0;
+let scoresList = [10, 25];
+let score = 20;
 
 scoresList.push(score);
 
@@ -11,15 +11,17 @@ function listHighScores(){
 
     for (i=0; i<scoresList.length; i++){
 
-        scoreElement.textContent = score;
+  //      scoreElement.textContent = score;
 
         var addScore = document.createElement("li");
 
-        addScore.textContent = scores[i];
+        addScore.textContent = scoresList[i];
         
         scoreElement.appendChild(addScore);
 
-
+     
     }
 
 };
+
+listHighScores();
