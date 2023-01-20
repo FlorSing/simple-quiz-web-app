326f03cc736b53c67f4669b2fa91b57f057b7c7f
FlorSing <fpsingcak@gmail.com>
2023-01-20 11:42:16 +0000

    show/hide questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,14 +1,29 @@
 let questionTitle = document.querySelector("#question-title");
 let choices = document.querySelector("#choices");
+let startButton = document.querySelector("#start");
+let questionsShow = document.querySelector("#questions");
 
 
 let question1 = ["What is used to style a webpage?", [' JavaScript', ' CSS', ' HTML']];
 
+//reminder: add toggle for question ("data") = show/hide  element.setAttribute("data-state", "animate")
+
+var mode = 'hide';
+
+startButton.addEventListener('click', function (){
+    event.preventDefault();
+    if (mode = 'hide'){
+        mode = 'show';
+        questionsShow.setAttribute('class', 'show');
+    }
+
+    }     
+    );
 
 
 questionTitle.textContent = question1[0];
 
-function renderChoices(){
+function listChoices(){
 
     for (i=0;i<question1[1].length;i++){
 
@@ -16,8 +31,13 @@ function renderChoices(){
         addListElement.textContent = question1[1][i];
         choices.appendChild(addListElement);
 
+        var button = document.createElement("button");
+        button.textContent = 'select';
+        addListElement.appendChild(button);
+        choices.appendChild(addListElement);
+
     }
 };
 
-renderChoices();
+listChoices();
 
